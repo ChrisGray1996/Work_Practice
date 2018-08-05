74e8a331bc61fa3c40ad78e2d29b45955cba02fc
DESKTOP-UAUFBE5\chris <christopher.gray1996@gmail.com>
2018-08-05 19:51:29 +0100

    Parse an XML file in C#

---
+++ XML_Practice/Parsing_XML_File/ParsingAnXMLFile/ParsingAnXMLFile/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace ParsingAnXMLFile
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.propertyGrid1 = new System.Windows.Forms.PropertyGrid();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // propertyGrid1
+            // 
+            this.propertyGrid1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.propertyGrid1.Location = new System.Drawing.Point(12, 237);
+            this.propertyGrid1.Name = "propertyGrid1";
+            this.propertyGrid1.Size = new System.Drawing.Size(611, 177);
+            this.propertyGrid1.TabIndex = 0;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(611, 212);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(635, 426);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.propertyGrid1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PropertyGrid propertyGrid1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ XML_Practice/Parsing_XML_File/ParsingAnXMLFile/ParsingAnXMLFile/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace ParsingAnXMLFile
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            LoadEmployees();
+        }
+
+        private void LoadEmployees()
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load("EmployeeData.xml");
+
+            //iterate through child elements in XML root element (doc.DocumentElement)
+            
+            foreach(XmlNode node in doc.DocumentElement)
+            {
+                string name = node.Attributes[0].Value;
+                int age = int.Parse(node["Age"].InnerText);
+                bool isMale = bool.Parse(node["isMale"].InnerText);
+                listBox1.Items.Add(new Employee(name, age, isMale));
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+            if (listBox1.SelectedIndex != -1)
+            {
+                propertyGrid1.SelectedObject = listBox1.SelectedItem;
+            }
+        }
+    }
+}
